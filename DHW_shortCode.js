b51ff0db372ecdca114cee9d49cd80f2b623b324
LyokoJeremie <lucheng989898@gmail.com>
2023-12-03 17:54:10 +0800

    return node

--- DHW_shortCode.js
+++ DHW_shortCode.js
@@ -7,14 +7,15 @@ window.DHW_sc = window.DHW_sc || {
       macroThis.passageObj
     );
   },
-  afterWidget: (widgetCode, widgetName, macroThis) => {
+  afterWidget: (widgetCode, widgetName, macroThis, node) => {
     return window.modSC2DataManager.getWikifyTracer().afterWidget(
       widgetCode,
       widgetName,
       macroThis.passageTitle || macroThis.passageObj.title,
-      macroThis.passageObj
+      macroThis.passageObj,
+      node,
     );
   },
 };
 // widgetCode = window.DHW_sc.beforeWidget(widgetCode, widgetName, macroThis);
-// window.DHW_sc.afterWidget(widgetCode, widgetName, macroThis);
+// window.DHW_sc.afterWidget(widgetCode, widgetName, macroThis, resFrag);
